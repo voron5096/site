3b4b63e11d70c2dd5be7de504ce87f8de3a245ab
voron5096 <70198995+voron5096@users.noreply.github.com>
2021-12-03 12:36:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+if (top.location.search == "") {
+  pass = prompt('Думаешь так просто?');
+  if (pass == 'baguvix') { }
+  else { top.location.href = "404" } //Адрес страниц на которую перейдет пользователь при ошибке 
+};         
