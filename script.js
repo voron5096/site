cf28c888c99aef23a87a8d6ab56457691a4859e1
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-26 18:30:26 +0000

    delete

--- script.js
+++
@@ -1,5 +0,0 @@
-if (top.location.search == "") {
-  pass = prompt('Думаешь так просто?');
-  if (pass == 'baguvix') { }
-  else { top.location.href = "404" } //Адрес страниц на которую перейдет пользователь при ошибке 
-};         
